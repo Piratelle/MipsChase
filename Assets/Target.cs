ff58340df92f4f87f744f9922cfc22b2acde6e88
Piratelle <eratelle@berkeley.edu>
2024-04-05 06:11:49 -0700

    Target hopping skeleton.

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Target : MonoBehaviour
 {
@@ -55,8 +57,33 @@ public class Target : MonoBehaviour
                 break;
             case eState.kHopStart:
                 // "hop... without going off the screen, AND avoid the player"
+                m_vHopStartPos = transform.position;
+                m_fHopStart = Time.time;
+                float fHopLength = m_fHopTime * m_fHopSpeed;
+
+                int nAttempts = 0;
+                do
+                {
+                    // try a new jump position
+                    // check to see if it's in bounds
+                    //      no -> skip to next try
+                    // check to see if it's too close to the player
+                    //      yes -> save as an in bounds option (max distance maybe?)
+                    //      no -> hop!
+                } while (nAttempts < m_nMaxMoveAttempts);
+
+                // calculate safe direction ?
+                //Vector3 vTryDirection = (m_vHopStartPos - m_player.transform.position).normalized;
+
+                m_nState = eState.kHop;
                 break;
             case eState.kHop:
+                // move along hop vector
+                float fStep = Time.deltaTime * m_fHopSpeed;
+                transform.position = Vector3.MoveTowards(transform.position, m_vHopEndPos, fStep);
+
+                // check for end of hop & state change
+                if (Vector3.Distance(transform.position, m_vHopEndPos) < 0.001f) m_nState = eState.kIdle;
                 break;
             default: // handles eState.kCaught
                 // "attaches itself to the player when it is caught"
